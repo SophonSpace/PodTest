edfd7b73d4a4086eb4da7d501806956031e2bf50
sophonspace <sophon@sophonspacedeMac-mini.local>
2018-03-29 09:06:44 +0800

    create new project

---
+++ YunoDemo/YunoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YunoDemo
+//
+//  Created by Sole on 2018/3/20.
+//  Copyright © 2018年 sole(sole.he@sophonspace.com). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
